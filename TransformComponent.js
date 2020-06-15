ecc9be8742b55b1b7595309615506aac83e2ef24
loldabigboi <loldabigboi@gmail.com>
2020-06-15 12:21:39 +1200

    added transform component

---
+++ TransformComponent.js
@@ -0,0 +1,15 @@
+class TransformComponent extends Component {
+
+    constructor(entityID, x, y, width, height, rot) {
+
+        super(entityID);
+
+        this.x = x;
+        this.y = y;
+        this.width = width;
+        this.height = height;
+        this.rot = rot;
+
+    }
+
+}
