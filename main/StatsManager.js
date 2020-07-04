88face88ed9ea5c743b4a8ad9ca81ae0b273fba9
loldabigboi <loldabigboi@gmail.com>
2020-07-04 17:32:09 +1200

    made stats manager more robust and added time tracker

--- main/StatsManager.js
+++ main/StatsManager.js
@@ -1,12 +1,12 @@
 class StatsManager {
 
     static getHighscore() {
-        return localStorage.getItem('highscore') || 0;
+        return parseInt(localStorage.getItem('highscore') || 0);
     }
 
     static setHighscore(newHighscore) {
-        const prevHighscore = localStorage.getItem('highscore');
-        if (prevHighscore == undefined || prevHighscore < newHighscore) {
+        const prevHighscore = parseInt(localStorage.getItem('highscore'));
+        if (isNaN(prevHighscore) || prevHighscore < newHighscore) {
             localStorage.setItem('highscore', newHighscore);
             return true;  // was a highscore
         }
@@ -14,39 +14,44 @@ class StatsManager {
     }
 
     static addEnemyKill() {
-        const curr = localStorage.getItem('kills') || 0;
+        const curr = parseInt(localStorage.getItem('kills') || 0);
         localStorage.setItem('kills', curr+1);
     }
     
     static getEnemiesKilled() {
-        return localStorage.getItem('kills') || 0;
+        return parseInt(localStorage.getItem('kills') || 0);
     }
 
     static addDeath() {
-        const curr = localStorage.getItem('deaths') || 0;
+        const curr = parseInt(localStorage.getItem('deaths') || 0);
         localStorage.setItem('deaths', curr+1);
     }
 
     static getDeaths() {
-        return localStorage.getItem('deaths') || 0;
+        return parseInt(localStorage.getItem('deaths') || 0);
     }
 
     static addCratePickup() {
-        const curr = localStorage.getItem('crates') || 0;
+        const curr = parseInt(localStorage.getItem('crates') || 0);
         localStorage.setItem('crates', curr+1);
     }
 
     static getCratesPickedUp() {
-        return localStorage.getItem('crates') || 0;
+        return parseInt(localStorage.getItem('crates') || 0);
     }
 
     static addShotFired() {
-        const curr = localStorage.getItem('shots') || 0;
+        const curr = parseInt(localStorage.getItem('shots') || 0);
         localStorage.setItem('shots', curr+1);
     }
 
     static getShotsFired() {
-        return localStorage.getItem('shots') || 0;
+        return parseInt(localStorage.getItem('shots') || 0);
+    }
+
+    static addTimePlayed(time) {
+        const curr = parseInt(localStorage.getItem('time_played') || 0);
+        localStorage.setItem('playtime', curr+time)
     }
 
 }
